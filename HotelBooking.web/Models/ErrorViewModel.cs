eb04161068684aa0fbfb9c876fbbb49f7431f95e
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-10-08 12:36:12 +0530

    Add project files.

---
+++ HotelBooking.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelBooking.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
