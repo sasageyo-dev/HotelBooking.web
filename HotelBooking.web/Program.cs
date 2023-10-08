3055b6102cae4803948049a663c033703b6d15c6
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-10-08 16:47:28 +0530

    Feat: Layers addedToProject

--- HotelBooking.web/Program.cs
+++ HotelBooking.web/Program.cs
@@ -1,7 +1,13 @@
+using HotelBooking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option=>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
