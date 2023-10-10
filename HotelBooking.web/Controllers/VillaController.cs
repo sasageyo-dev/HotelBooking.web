6aa7be5e66513cdc296f956809dff4666820fd45
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-10-10 10:58:33 +0530

    Feat: VillaController added

---
+++ HotelBooking.web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using HotelBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Web.Controllers
+{
+    
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+    }
+}
