af82697dfd2b7224ef6790b484292f96006641fa
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-10-13 01:54:29 +0530

    Feat: On-going villa controller

--- HotelBooking.domain/Entities/Villa.cs
+++ HotelBooking.domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,18 @@ namespace HotelBooking.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [DisplayName("Villa Name")]
         public required string Name { get; set; }
         public string? Description { get; set; }
 
+        [DisplayName("Price per night")]
         public double Price { get; set; }
 
         public int Sqft { get; set; }
 
         public int Occupancy { get; set; }
 
+        [DisplayName("Image Url")]
         public string? ImageUrl { get; set; }
 
         public DateTime? Created_Date { get; set; }
--- HotelBooking.web/Controllers/VillaController.cs
+++ HotelBooking.web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using HotelBooking.Infrastructure.Data;
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBooking.Web.Controllers
@@ -15,6 +16,19 @@ namespace HotelBooking.Web.Controllers
         {
             var villa = _db.Villas.ToList();
             return View(villa);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            _db.Villas.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
